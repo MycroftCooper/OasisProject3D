8fb19dd02dca4953aadff9aa6d819d7eace7604c
风徐 <2661347713@qq.com>
2023-02-19 21:34:31 +0800

    GComponent简单包装
    包装成UIComponent，待完善；后续还要包装一个Dialog

---
+++ Assets/GameMain/Scripts/UI/FGUI/UIComponent.cs
@@ -0,0 +1,33 @@
+﻿using FairyGUI;
+
+namespace OasisProject3D.UI
+{
+    public abstract class UIComponent : GComponent
+    {
+        #region
+        protected abstract void Init();
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            /// TODO
+            UnRegisterEvent();
+            OnDispose();
+        }
+
+        protected abstract void Update();
+
+        protected abstract void RegisterEvent();
+
+        protected virtual void UnRegisterEvent()
+        {
+            /// TODO
+        }
+
+        protected virtual void OnDispose()
+        {
+            /// TODO
+        }
+        #endregion
+    }
+}
