fe9386c49c3116272ad30456923544b879eea2cb
烤麸 <2570150161@qq.com>
2023-03-08 02:03:42 +0800

    [优化] 分出部分操作到游戏主流程

--- Assets/GameMain/Scripts/Procedures/Base/GameLoadingProcedure.cs
+++ Assets/GameMain/Scripts/Procedures/Base/GameLoadingProcedure.cs
@@ -4,37 +4,40 @@ using OasisProject3D.UI.GameEntryUIPackage;
 using QuickGameFramework.Procedure;
 using QuickGameFramework.Runtime;
 
-public class GameLoadingProcedure : Procedure {
-    private bool _isLoadCompleted;
-    Timer _timer;
-    private ProgressBar1 _progressBar;
-    private AssetLoadProgress _assetLoadProgress;
-    
-    protected override void OnEnter(params object[] parameters) {
-        _progressBar = GameEntry.UIMgr.GetUIInstance<LoadingPage>(nameof(LoadingPage)).ProgressBar;
-        _assetLoadProgress = BlockFactory.Instance.PreLoadAsset();
-        _assetLoadProgress += GameEntry.UIMgr.PreLoadAsset();
-        _assetLoadProgress.Completed += () => _isLoadCompleted = true;
-        
-        _progressBar.value = 0;
-        _timer = Timer.Register(3, null);
-    }
+namespace OasisProject3D.Procedures {
+    public class GameLoadingProcedure : Procedure {
+        private bool _isLoadCompleted;
+        Timer _timer;
+        private ProgressBar1 _progressBar;
+        private AssetLoadProgress _assetLoadProgress;
 
-    protected override void OnUpdate(float intervalSeconds) {
-        if (_isLoadCompleted && _timer.isCompleted) {
-            Exit();
-            return;
+        protected override void OnEnter(params object[] parameters) {
+            _progressBar = GameEntry.UIMgr.GetUIInstance<LoadingPage>(nameof(LoadingPage)).ProgressBar;
+            _assetLoadProgress = BlockFactory.Instance.PreLoadAsset();
+            _assetLoadProgress += GameEntry.UIMgr.PreLoadAsset();
+            _assetLoadProgress.Completed += () => _isLoadCompleted = true;
+
+            _progressBar.value = 0;
+            _timer = Timer.Register(3, null);
         }
-        _progressBar.Update(_timer.GetTimeElapsed()/_timer.duration *100);
-        // _progressBar.Update(_assetLoadProgress.Progress); 现在加载太快了，但总得演一下吧
-    }
 
-    protected override void OnExit() {
-        GameEntry.ChangeSceneAsync("MainGameScene", () => {
-            GameEntry.ModuleMgr.CreateModule<MapManager>();
-            GameEntry.AssetMgr.UnloadAssets();
-        });
-    }
+        protected override void OnUpdate(float intervalSeconds) {
+            if (_isLoadCompleted && _timer.isCompleted) {
+                Exit();
+                return;
+            }
 
-    protected override void OnDestroy() { }
+            _progressBar.Update(_timer.GetTimeElapsed() / _timer.duration * 100);
+            // _progressBar.Update(_assetLoadProgress.Progress); 现在加载太快了，但总得演一下吧
+        }
+
+        protected override void OnExit() {
+            GameEntry.ChangeSceneAsync("MainGameScene", () => {
+                GameEntry.AssetMgr.UnloadAssets();
+                GameEntry.ProcedureMgr.StartProcedure(typeof(MainGameProcedure));
+            });
+        }
+
+        protected override void OnDestroy() { }
+    }
 }
---
+++ Assets/GameMain/Scripts/Procedures/Base/MainGameProcedure.cs
@@ -0,0 +1,23 @@
+using OasisProject3D.MapSystem;
+using QuickGameFramework.Procedure;
+using QuickGameFramework.Runtime;
+
+namespace OasisProject3D.Procedures {
+    public class MainGameProcedure : Procedure {
+        protected override void OnEnter(params object[] parameters) {
+            GameEntry.ModuleMgr.CreateModule<MapManager>();
+        }
+
+        protected override void OnUpdate(float intervalSeconds) {
+            
+        }
+
+        protected override void OnExit() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnDestroy() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
